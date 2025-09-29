7249bcf9635a218c870db77557b13351d4ba1fe2
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-09-29 16:22:59 -0300

    chore: configuração do data source do typeorm

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port:
+    process.env.DATABASE_PORT == undefined
+      ? 3306
+      : parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [__dirname + '/../**/*.entity.ts'],
+  migrations: [__dirname + '/../migrations/*.ts'],
+});
+
+export default AppDataSource;
