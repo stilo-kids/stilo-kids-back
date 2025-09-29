0586501761e0d14d78f002b1401c57e8fc92f0b9
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-09-29 16:21:08 -0300

    feat: script para cli do projeto

---
+++ helper.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+const { spawn } = require('child_process');
+const os = require('os');
+
+// Detecta se é Windows
+const npmCmd = os.platform() === 'win32' ? 'npm.cmd' : 'npm';
+
+const [,, command, ...args] = process.argv;
+
+if (!command) {
+  console.error('Erro: nenhum comando fornecido.');
+  process.exit(1);
+}
+
+function runCommand(cmdArgs) {
+  console.log('Executando:', [npmCmd, 'run', 'typeorm', '--', ...cmdArgs].join(' '));
+  const proc = spawn(npmCmd, ['run', 'typeorm', '--', ...cmdArgs], { stdio: 'inherit' });
+
+  proc.on('exit', code => process.exit(code));
+}
+
+switch(command) {
+  case 'migration:run':
+    runCommand(['migration:run', '-d', 'src/config/typeorm.config.ts']);
+    break;
+
+  case 'migration:revert':
+    runCommand(['migration:revert', '-d', 'src/config/typeorm.config.ts']);
+    break;
+
+  case 'migration:create':
+    if (args.length === 0) {
+      console.error('Erro: migration:create requer o nome da migration.');
+      process.exit(1);
+    }
+    runCommand(['migration:create', `src/migrations/${args[0]}`]);
+    break;
+
+  case 'migration:generate':
+    if (args.length === 0) {
+      console.error('Erro: migration:generate requer o nome da migration.');
+      process.exit(1);
+    }
+    runCommand(['migration:generate', `src/migrations/${args[0]}`, '-d', 'src/config/typeorm.config.ts']);
+    break;
+
+  case 'entity:create':
+    if (args.length < 2) {
+      console.error('Erro: entity:create requer <módulo> e <entidade>.');
+      process.exit(1);
+    }
+    const [moduleName, entityName] = args;
+    const path = `src/${moduleName}/entities/${entityName}.entity`;
+    runCommand(['entity:create', path]);
+    break;
+
+  default:
+    console.error('Comando desconhecido:', command);
+    process.exit(1);
+}
