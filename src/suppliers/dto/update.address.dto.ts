d8f25f33c165874510ab31a21adfc23d33c41364
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-10-06 09:03:28 -0300

    feat: validação do formulário de atualização de endereço

--- src/suppliers/dto/update.address.dto.ts
+++ src/suppliers/dto/update.address.dto.ts
@@ -1,38 +1,6 @@
-import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateAddressDto {
-  @ApiProperty({
-    description: 'Logradouro',
-    required: true,
-    example: 'Rua das Flores',
-  })
-  readonly street: string;
+import { PartialType } from '@nestjs/swagger';
+import { CreateAddressDto } from './create.address.dto';
 
-  @ApiProperty({
-    description: 'Cidade',
-    required: true,
-    example: 'São Paulo',
-  })
-  readonly city: string;
-
-  @ApiProperty({
-    description: 'Bairro',
-    required: true,
-    example: 'Jardim das Flores',
-  })
-  readonly neighborhood: string;
-
-  @ApiProperty({
-    description: 'Número',
-    required: true,
-    example: '123',
-  })
-  readonly number: string;
-
-  @ApiProperty({
-    description: 'ID do Fornecedor associado a este endereço',
-    required: true,
-    example: 1,
-  })
-  readonly supplierId: number;
+export class UpdateAddressDto extends PartialType(CreateAddressDto) {
 }
