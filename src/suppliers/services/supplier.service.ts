6f4d40c42de80247f97d622af1ed0162b083e614
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-10-06 09:07:46 -0300

    feat: remover/adicionar os endereços/telefones de um fornecedor na atualização

--- src/suppliers/services/supplier.service.ts
+++ src/suppliers/services/supplier.service.ts
@@ -4,12 +4,18 @@ import { Supplier } from '../entities/supplier.entity';
 import { Repository } from 'typeorm';
 import CreateSupplierDto from '../dto/create.supplier.dto';
 import UpdateSupplierDto from '../dto/update.supplier.dto';
+import { Address } from '../entities/address.entity';
+import { Telephone } from '../entities/telephone.entity';
 
 @Injectable()
 export class SupplierService {
   constructor(
     @InjectRepository(Supplier)
     private readonly supplierRepository: Repository<Supplier>,
+    @InjectRepository(Address)
+    private readonly addressRepository: Repository<Address>,
+    @InjectRepository(Telephone)
+    private readonly telephoneRepository: Repository<Telephone>,
   ) {}
 
   findAll(): Promise<Supplier[]> {
@@ -36,8 +42,38 @@ export class SupplierService {
     return this.supplierRepository.save(supplier);
   }
 
-  update(id: number, updateSupplierDto: UpdateSupplierDto): Promise<Supplier> {
-    return this.supplierRepository.save({ id, ...updateSupplierDto });
+  async update(
+    id: number,
+    updateSupplierDto: UpdateSupplierDto,
+  ): Promise<Supplier> {
+    const supplier = await this.supplierRepository.findOne({
+      where: { id },
+      relations: { addresses: true, telephones: true },
+    });
+
+    if (!supplier) throw new Error('Supplier not found');
+
+    console.log(updateSupplierDto);
+    if (updateSupplierDto.addresses !== undefined) {
+      await this.addressRepository.softRemove(supplier.addresses);
+      supplier.addresses = [];
+      supplier.addresses = (updateSupplierDto.addresses || []).map((address) =>
+        this.addressRepository.create({ ...address, supplier }),
+      );
+    }
+
+    if (updateSupplierDto.telephones !== undefined) {
+      await this.telephoneRepository.softRemove(supplier.telephones);
+      supplier.telephones = [];
+      supplier.telephones = (updateSupplierDto.telephones || []).map(
+        (telephone) =>
+          this.telephoneRepository.create({ ...telephone, supplier }),
+      );
+    }
+
+    Object.assign(supplier, updateSupplierDto);
+
+    return this.supplierRepository.save(supplier);
   }
 
   async remove(supplier: Supplier): Promise<void> {
